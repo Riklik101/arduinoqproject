f5996f39cbf946d7e32383085afb681720638b3c
Eklavya Agrawal <iamanswer123472@gmail.com>
2025-10-12 14:26:23 -0500

    continue

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
