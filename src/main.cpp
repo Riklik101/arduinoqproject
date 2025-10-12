ab784cce2595db5df44f2e6d503ab4bbd0a0ec3e
Eklavya Agrawal <iamanswer123472@gmail.com>
2025-10-12 14:52:27 -0500

    firstprint

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,10,9,8,7);
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  lcd.begin(16,2);
 }
 
 void loop() {
-  
+  if (Serial.available()) {
+    
+    lcd.write(Serial.read());
+  }
 }
 
 // put function definitions here:
