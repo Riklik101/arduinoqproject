595b6e75c99cea872083d05a2a7dea6acbabb4c4
Eklavya Agrawal <iamanswer123472@gmail.com>
2025-10-12 19:15:37 -0500

    work

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,24 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12,11,10,9,8,7);
-
+const int clearPin = 6;
+char serialInput;
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   Serial.begin(9600);
   lcd.begin(16,2);
+  pinMode(clearPin, INPUT);
 }
 
 void loop() {
   if (Serial.available()) {
-    
-    lcd.write(Serial.read());
+    serialInput = Serial.read();
+    lcd.write(serialInput);
+    Serial.println("Character(s) written to LCD");
+  } else if (digitalRead(clearPin)==HIGH) {
+    lcd.clear();
   }
 }
 
